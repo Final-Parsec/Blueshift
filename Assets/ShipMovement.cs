f36d2b4ebf54d7c320bfd996341e3784baa3199d
Baer <baer.bradford@gmail.com>
2014-12-29 21:54:45 -0600

    Level 1 environment and ship movement.
    Level 1 has sand and a daylight skybox to look more like desert
    environment (Somalia).
    Ship movement works on desktop with arrow keys and accelerometer on
    mobile.

--- Assets/ShipMovement.cs
+++ Assets/ShipMovement.cs
@@ -1,32 +1,85 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class ShipMovement : MonoBehaviour {
-
-    void Move(float x, float y, float z)
+public class ShipMovement : MonoBehaviour 
+{
+    /// <summary>
+    /// Decelerates the ship and raises on y-axis. 
+    /// </summary>
+    void Ascend()
     {
-        // Do not let ship go below environment.
-        if (transform.position.y < 3)
-        {
-            y = Mathf.Abs(y);
-        }
-
-        transform.Translate(x, y, z);
+        transform.Translate(0, .5f, .25f);
+    }
+    
+    /// <summary>
+    /// Steers ship to the left.
+    /// </summary>
+    void BankLeft()
+    {
+        transform.Translate(-.5f, 0, 0);
     }
 
-	// Use this for initialization
-	void Start () {
-	
+    /// <summary>
+    /// Steers ship to the right.
+    /// </summary>
+    void BankRight()
+    {
+        transform.Translate(.5f, 0, 0);
+    }
+
+    /// <summary>
+    /// Accelerates ship and lowers on y-axis.
+    /// </summary>
+    void Dive()
+    {
+        var groundLevel = transform.position.y < 3;
+
+        // Do not let ship go below 3.
+        transform.Translate(0, groundLevel ? 0 : -.5f, groundLevel ? 1 : 1.5f);
+    }
+
+    /// <summary>
+    /// Makes the ship go forward.
+    /// </summary>
+    void GoForward()
+    {
+        transform.Translate(0, 0, 1);
+    }
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+	void Start ()
+    {
+        
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        transform.position += transform.forward * Time.deltaTime;
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.DownArrow) == true ||
+            Input.acceleration.y > -.25)
+        {
+            Dive();
+        }
+        else if (Input.GetKey(KeyCode.UpArrow) == true ||
+            Input.acceleration.y < -.85)
+        {
+            Ascend();
+        }
+        else
+        {
+            GoForward();
+        }
 
-        var inputX = Input.acceleration.x;
-        var inputY = -Input.acceleration.y;
-        var inputZ = -Input.acceleration.z;
-
-        Move(inputX, inputY, inputZ);        
+        if (Input.GetKey(KeyCode.LeftArrow) == true ||
+            Input.acceleration.x < -.25)
+        {
+            BankLeft();
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) == true ||
+            Input.acceleration.x > .25)
+        {
+            BankRight();
+        }
 	}
 }
