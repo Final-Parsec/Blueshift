d59c25f9d5e49164f5aa7087d79113110f86ebd7
Baer <baer.bradford@gmail.com>
2015-01-05 23:30:13 -0600

    Added scene loading checkpoint & mission 2.

---
+++ Assets/CheckpointBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointBehavior : MonoBehaviour
+{
+
+    public bool loadScene = false;
+    public string sceneToLoad = "";
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (loadScene && !string.IsNullOrEmpty(sceneToLoad))
+        {
+            Application.LoadLevel(sceneToLoad);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
