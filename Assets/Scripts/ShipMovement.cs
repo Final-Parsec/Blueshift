53458aeb0ffdb2f65b029064312c17598ba76c29
finalparsec <finalparsec@gmail.com>
2015-01-10 19:46:50 -0600

    Added rigid body to terrain. Stops player from clipping through it. May cause performance problems on mobile IDK.

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -2,6 +2,8 @@
 
 public class ShipMovement : MonoBehaviour
 {
+    public static ShipMovement shipMovement;
+
     /// <summary>
     /// Decelerates the ship and raises on y-axis. 
     /// </summary>
@@ -37,10 +39,35 @@ public class ShipMovement : MonoBehaviour
     /// </summary>
     void Dive()
     {
-        var groundLevel = transform.position.y < 1;
+        bool pastBottomScreenEdge = Camera.main.WorldToScreenPoint(transform.position).y <= 0;
 
         // Do not let ship go below the ground.
-        transform.Translate(0, groundLevel ? 0 : -.5f, 0);
+        transform.Translate(0, pastBottomScreenEdge ? 0 : -.5f, 0);
+
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) ||
+            (Input.acceleration.y > -.25 && Input.acceleration.y != 0))
+        {
+            Dive();
+        } else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) ||
+                   Input.acceleration.y < -.85)
+        {
+            Ascend();
+        }
+        
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) ||
+            Input.acceleration.x < -.25)
+        {
+            BankLeft();
+        } else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) ||
+                   Input.acceleration.x > .25)
+        {
+            BankRight();
+        }
     }
 
     /// <summary>
@@ -48,30 +75,24 @@ public class ShipMovement : MonoBehaviour
     /// </summary>
     void Start()
     {
-        
+        shipMovement = this;
     }
     
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) ||
-            (Input.acceleration.y > -.25 && Input.acceleration.y != 0))
-        {
-            Dive();
-        } else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) ||
-            Input.acceleration.y < -.85)
-        {
-            Ascend();
-        }
 
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) ||
-            Input.acceleration.x < -.25)
-        {
-            BankLeft();
-        } else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) ||
-            Input.acceleration.x > .25)
-        {
-            BankRight();
-        }
+
+//        RaycastHit rcHit;
+//        Ray ray = Vector3.down;
+//        if (Physics.Raycast(transform.position, ray, rcHit))
+//        {
+//            if (rcHit.collider.tag != "terrain")
+//            {
+//
+//                transform.position = 
+//            }
+//        }
     }
+
 }
