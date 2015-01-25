88cf543ac1c249c74d6a7c74d3620745015196b4
TheMattBauer <matthewbauer21@gmail.com>
2015-01-25 12:30:03 -0600

    Adding back my fixed changes

---
+++ Assets/Scripts/Enemies/BobbingEnemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BobbingEnemy : FlyingEnemy {
+
+	public int deltaYDistance;	// distance to bob
+	public float bobbingSpeed;
+	private float originalY;
+	private int direction = 1;	// direction of bobbing.
+
+	protected override IEnumerator Active()
+	{	
+		while (TagsAndEnums.GetSqrDistance(transform.root.position, ShipMovement.shipMovement.transform.position) < activeRange*activeRange)
+		{
+			if(transform.root.position.y >= originalY + deltaYDistance || transform.root.position.y <= originalY - deltaYDistance)
+			{
+				direction = -direction;
+				transform.root.position = new Vector3(transform.root.position.x,
+				                                      originalY + (deltaYDistance - .1f) * direction,
+				                                      transform.root.position.z);
+			}
+			transform.root.position = new Vector3(transform.root.position.x,
+			                                      transform.root.position.y - direction * Time.deltaTime * bobbingSpeed,
+			                                      transform.root.position.z);
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	protected override void Start()
+	{
+		originalY = transform.root.position.y;
+		base.Start();
+	}
+}
---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemy : MonoBehaviour {
+	public TagsAndEnums.ProjectileType projectileType;
+	public List<Transform> muzzlePoints;
+	public bool shootFromEveryMuzzle;
+	protected int currentMuzzlePoint = 0;
+	protected GameObject target;
+
+	protected Transform GetMuzzlePoint()
+	{
+		Transform point = muzzlePoints[currentMuzzlePoint];
+		currentMuzzlePoint++;
+		if(currentMuzzlePoint >= muzzlePoints.Count)
+			currentMuzzlePoint = 0;
+		return point;
+	}
+
+	protected virtual void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == TagsAndEnums.player)
+		{
+			target = other.gameObject;
+		}
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == TagsAndEnums.player)
+			target = null;
+	}
+}
