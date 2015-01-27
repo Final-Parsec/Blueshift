5c54dc479ca6716299785f9c001ba38b999516ce
TJ Brosnan <pudey33@yahoo.com>
2015-01-26 22:46:35 -0600

    We'll clean up your mess as long as you finish the job.
    Cutscene1 added.

---
+++ Assets/Scripts/Cutscene/Cutscenes IO.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class CutscenesIO{
+	string[] lines;
+	public CutscenesIO (string filename) 
+	{
+		StreamReader reader = File.OpenText(@"Assets\Cutscenes\Dialogue\" + filename + ".txt");
+		string text = reader.ReadToEnd ();
+		lines = text.Split('\n');
+	}
+
+	//Returns the specified line of dialogue.
+	public string GetLine(int dialogue_number)
+	{
+		if (lines.Length > dialogue_number) 
+		{
+			string[] str = lines [dialogue_number].Split ('@');
+			return str [1];
+		}
+		return "You outa lines, nigga.";
+	}
+
+	//Returns the specified name attached to dialogue.
+	public string GetName(int dialogue_number)
+	{
+		if (lines.Length > dialogue_number) 
+		{
+			string[] str = lines [dialogue_number].Split ('@');
+			return str [0];
+		}
+		return "You outa lines, nigga.";
+		
+	}
+}
---
+++ Assets/Scripts/Cutscene/Cutscenes.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Cutscenes : MonoBehaviour{
+	CutscenesIO io;
+	int curLine = 0;
+	Text dialogue;
+	Text speakerName;
+	bool talking = false;
+	private AudioSource audiosource;
+
+	void Start(){
+		io = new CutscenesIO (Application.loadedLevelName);
+		dialogue = GameObject.Find ("Dialogue").GetComponent<Text>();
+		speakerName = GameObject.Find ("CharName").GetComponent<Text> ();
+		//audiosource = GetComponent<AudioSource> ();
+	}
+
+	public void NextLine()
+	{
+		SetSpeaker (io.GetName(curLine));
+		StartCoroutine(SayDialogue(io.GetLine (curLine)));
+		curLine++;
+	}
+
+	IEnumerator SayDialogue (string toSay) {
+		talking = true;
+		
+		for (int i = 0; i <= toSay.Length; i ++) {
+			dialogue.text = toSay.Substring(0, i);
+			//audiosource.Play ();
+			Debug.Log ("play");
+			yield return new WaitForSeconds (0.02f);
+		}
+		
+		//while (!Input.GetMouseButton(0))
+			//yield return new WaitForEndOfFrame();
+
+		talking = false;
+
+	}
+
+	void Update()
+	{
+		if (Input.anyKeyDown)
+		{
+			NextLine ();
+		}
+	}
+
+	void SetSpeaker(string name){
+		speakerName.text = name;
+	}
+
+
+
+}
