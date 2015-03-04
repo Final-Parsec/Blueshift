5724be8468cb520cfb23c9d06b03be7f338afbd1
TJ Brosnan <pudey33@yahoo.com>
2015-03-03 20:08:25 -0600

    why
    why

---
+++ Assets/Scripts/Main Menu/MenuCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject _earth = GameObject.Find ("Earth");
+
+		Transform target = _earth.GetComponent<Transform>();
+		//gameObject.GetComponent<> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
