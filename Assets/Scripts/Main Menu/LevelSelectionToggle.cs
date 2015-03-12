88821641bde5d0840074e59018ea4eef826398ea
Baer <baer.bradford@gmail.com>
2015-03-11 19:49:24 -0500

    I think I fixed the anchoring on the main menu.
    Also moved assets into the right folders.

---
+++ Assets/Scripts/Main Menu/LevelSelectionToggle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelectionToggle : MonoBehaviour
+{
+	private GameObject canvas;
+	private GameObject[] missionStartButtons;
+
+	/// <summary>
+	/// Use this for initialization.
+	/// </summary>
+	void Start()
+	{
+		canvas = GameObject.Find("Canvas");
+		missionStartButtons = GameObject.FindGameObjectsWithTag(TagsAndEnums.levelSelector);
+	}
+
+	/// <summary>
+	/// Update is called once per frame.
+	/// </summary>
+	void Update ()
+	{
+		if (Input.anyKey)
+		{
+			canvas.SetActive(false);
+			
+			foreach (var missionStartButton in missionStartButtons)
+			{
+				missionStartButton.SetActive(true);
+			}
+		}
+	}
+}
