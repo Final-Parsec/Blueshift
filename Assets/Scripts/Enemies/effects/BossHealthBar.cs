55a6470fb560776d5271fda1d79c7b52c4159aef
themattbauer <matthewbauer21@gmail.com>
2015-03-14 01:26:13 -0500

    added a player health bar

--- Assets/Scripts/Enemies/effects/BossHealthBar.cs
+++ Assets/Scripts/Enemies/effects/BossHealthBar.cs
@@ -2,44 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class BossHealthBar : MonoBehaviour {
+public class BossHealthBar : HealthBar {
 
     public Animator animator;
-    private float startingHeight;
-    private float startingYPosition;
-    private RectTransform rectTransform;
 
-	void Start () 
+    void Awake () 
     {
         animator = GetComponentInParent<Animator>();
-        rectTransform = GetComponent<RectTransform>();
-
-        startingHeight = rectTransform.sizeDelta.y;
-        rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, 0);
-
-        startingYPosition = rectTransform.localPosition.y;
-	}
-	
-	public void UpdateHealthBar (int health, int maxHealth) 
-    {
-        float healthRatio = ((float)health) / ((float)maxHealth);
-        float newHeight = startingHeight * (1 - healthRatio);
-
-        StartCoroutine(MoveBar(newHeight));
-	}
-
-    IEnumerator MoveBar(float targetHeight)
-    {
-        while (rectTransform.sizeDelta.y < targetHeight)
-        {
-            rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, rectTransform.sizeDelta.y + 1);
-            
-            rectTransform.localPosition = new Vector3(rectTransform.localPosition.x,
-                                                      startingYPosition + ((startingHeight - rectTransform.sizeDelta.y) / 2.0f),
-                                                      rectTransform.localPosition.z);
-
-            yield return new WaitForEndOfFrame();
-        }
     }
 
     public void SwoopIn()
--- Assets/Scripts/ShipHealth.cs
+++ Assets/Scripts/ShipHealth.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class ShipHealth : MonoBehaviour, Health {
 	private Animator animator;
 	public int MaxHealth{get; set;}
+    public HealthBar healthBar;
 
 	private bool isDying = false;
+
 	public bool IsDying
 	{
 		get
@@ -27,26 +29,29 @@ public class ShipHealth : MonoBehaviour, Health {
 		}
 		set
 		{
-			_health = value;
-			
+
 			if(isDying)
 				return;
 
 			if (_health <= 0)
 			{
+                _health = 0;
 				Death();
 				//Destroy(gameObject);
 			}
 			else
 			{
+                _health = value;
 				AnimateHit();
 			}
+            healthBar.UpdateHealthBar(_health, MaxHealth);
 		}
 	}
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
+        MaxHealth = Health;
 	}
 	
 	void AnimateHit()
