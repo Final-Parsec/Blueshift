0a62de0cce0b0faa5359bbb036bfc16d6b00101e
Baer <baer.bradford@gmail.com>
2015-05-10 15:11:00 -0500

    Random sound effects for in game dialogue.
    I prefer it to the repeated thuds I copied from the cutscene stuff.

--- Assets/Scripts/InGameDialogue.cs
+++ Assets/Scripts/InGameDialogue.cs
@@ -4,14 +4,18 @@ using UnityEngine.UI;
 
 public class InGameDialogue : MonoBehaviour
 {
-    private AudioSource audiosource;
+    public AudioClip[] dialogueAudioClips;
+    
+    private AudioSource audioSource;
     private Image background;
     private int currentLine;
     private Text dialogue;
+    private AudioClip previousAudioClip;
     public static InGameDialogue Instance { get; private set; }
 
     private void Start()
     {
+        this.audioSource = this.GetComponent<AudioSource>();
         this.background = GameObject.Find("Dialogue Background").GetComponent<Image>();
         this.background.color = new Color(0, 0, 0, 0);
         this.dialogue = GameObject.Find("Dialogue").GetComponent<Text>();
@@ -33,18 +37,21 @@ public class InGameDialogue : MonoBehaviour
             this.background.color = new Color(current.r, current.g, current.b, current.a + .02f);
             yield return new WaitForEndOfFrame();
         }
-        
+
+        var randomAudioClip = this.dialogueAudioClips[Random.Range(0, this.dialogueAudioClips.Length)];
+        while (randomAudioClip == this.previousAudioClip)
+        {
+            // Our random clip was the same as last time. Pick a new one so we don't duplicate.
+            randomAudioClip = this.dialogueAudioClips[Random.Range(0, this.dialogueAudioClips.Length)];
+        }
+        this.previousAudioClip = randomAudioClip;
+        this.audioSource.clip = randomAudioClip;
+        this.audioSource.Play();
+
         // Display text character by character.
         while (coroutineCharacterCount <= toSay.Length)
         {
             this.dialogue.text = toSay.Substring(0, coroutineCharacterCount);
-
-            // Play audio for characters that aren't whitespace.
-            if (coroutineCharacterCount > 0 &&
-                (toSay[coroutineCharacterCount - 1] != ' ' || toSay[coroutineCharacterCount - 1] != '\n'))
-            {
-                this.GetComponent<AudioSource>().Play();
-            }
 
             if ((coroutineCharacterCount > 0) &&
                 (toSay[coroutineCharacterCount - 1] == '.' || toSay[coroutineCharacterCount - 1] == '?' ||
